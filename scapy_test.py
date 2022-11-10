7f638c6045d045fde21e73b0c68c4aadd395b173
Nivispluma <matzeminecraft@gmail.com>
2022-11-10 20:54:50 +0100

    ADD: Layer 2 Packet test

--- scapy_test.py
+++ scapy_test.py
@@ -1,17 +1,23 @@
 from scapy.all import *
 
 
-def test(src_ip, dest_ip, dest_port):
+def test(src_ip, dest_ip, dest_port, network_broadcast):
     packet = IP(src=src_ip, dst=dest_ip) / UDP(dport=dest_port) / Raw(load="abc")
 
-    EtherDA()
+
+    ether_pack = Ether(dst="ff:ff:ff:ff:ff:ff")/Dot1Q(vlan=1)/IP(src=src_ip, dst=dest_ip)/ UDP(dport=dest_port) / Raw(load="cooler_pack")
+
+    
+ 
     packet.show()
     for i in range(10):
-        sendp(packet)
+        send(packet)
+        sendp(ether_pack)
 
 
 if __name__ == "__main__":
     ip_src = str(sys.argv[1])
     ip_dest = str(sys.argv[2])
     port_dest = int(sys.argv[3])
-    test (ip_src, ip_dest,port_dest)
+    broadcast_ip = str(sys.argv[4])
+    test (ip_src, ip_dest,port_dest, broadcast_ip)
